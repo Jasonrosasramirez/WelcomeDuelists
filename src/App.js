de0825722a8ef2f6b33e7b547d0589d4dc678e57
Jason <jrosasrjf@gmail.com>
2022-03-10 19:38:41 -0600

    added in react router node module

--- src/App.js
+++ src/App.js
@@ -1,4 +1,6 @@
 import React, {useState} from "react";
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+
 
 // Importing other pages files 
 import PageTemplate from "./pages/PageTemplate.js";
@@ -7,7 +9,7 @@ import Home from './pages/Home.js';
 
 function App() {
   
-// The return can only export 1 element. You can bundle multiple elements using a cluster
+  // The return can only export 1 element. You can bundle multiple elements using a cluster
   return ( 
     <displayOnScreen>
       <PageTemplate/>
