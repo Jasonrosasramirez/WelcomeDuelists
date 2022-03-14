3471b80b70f1e3b5edb1a0dff695ce7c80594333
Jason <jrosasrjf@gmail.com>
2022-03-14 00:05:54 -0500

    practiced with the CSS layout and JSON fetching

--- src/pages/GOATFormatPage.js
+++ src/pages/GOATFormatPage.js
@@ -6,7 +6,13 @@ import '../App.css';
 import LOB from '../BoosterSets/00_LOB/Legend of Blue Eyes White Dragon (LOB).png';
 import TriHornedDragon from '../BoosterSets/00_LOB/Monsters/TriHornedDragon.png';
 import BlueEyesWhiteDragon from '../BoosterSets/00_LOB/Monsters/BlueEyesWhiteDragon.png';
+import DarkAttribute from "../Images/DARK.png";
+import STAR from "../Images/STAR.png";
 
+
+// import CSS Styles 
+import './GOATFormatPage.css';
+import { dark } from '@material-ui/core/styles/createPalette';
 
 function GOATFormatPage () {
 
@@ -29,8 +35,100 @@ function GOATFormatPage () {
                 <h2> Legal Sets </h2>
 
                 <h3> Proof of Concept </h3>
+                
+                <br></br>
 
-                <section className='boosterPack' id='LOB'>
+                <section>
+
+                    <div className='monsterInfo' id='TriHorned_Dragon'>
+
+                        
+                        
+                        <img className="cardImage" src={"https://storage.googleapis.com/ygoprodeck.com/pics/39111158.jpg"}></img>
+
+
+                        <div className='cardInfo'>
+                            
+                            <div className='cardTop'>
+                                <div>
+                                    <h2 className='cardName'>TriHorned_Dragon</h2>
+                                </div>
+                                
+                                <div classname = "attribute">
+                                    <img className = "attribute_Image" src={DarkAttribute}></img>
+                                </div>
+                            </div>
+
+                            <div className='cardSemiTop'>
+                                <img className = "Star_Image" src={STAR}></img>
+                                <h3>Level 8</h3>
+                            </div>
+                            
+                            <div className='cardMiddle'>
+
+                                <p className='cardType'>[Dragon]</p>
+                                <p className='flavorText'>An unworthy dragon with three sharp horns sprouting from its head.</p>
+                            </div>
+                            
+                            <div className="cardBottom">
+                                <p> ATT/2850 DEF/2350 </p>
+                                <p> R </p>
+                            </div>
+                            
+
+                            
+                                                        
+                        </div>
+
+                    </div>
+                    
+                </section>
+
+            </div>
+
+        </div>
+
+    );
+}
+
+
+export default GOATFormatPage; // I will export to App.js
+
+
+/*Legal Sets: 
+
+Legend of Blue Eyes White Dragon (LOB), 
+Yugi and Kaiba Starter Decks (SDY/SDK), 
+Metal Raiders (MRD), Magic/Spell Ruler (MRL/SRL), 
+Pharaoh's Servant (PSV), 
+Joey and Pegasus Starter Decks (SDJ/SDP), 
+Labyrinth of Nightmare (LON), 
+Legacy of Darkness (LOD), 
+Pharaonic Guardian (PGD), 
+Magician's Force (MFC), 
+Dark Crisis (DCR), 
+Yugi and Kaiba Evolution Decks (SYE/SKE), 
+Invasion of Chaos (IOC), 
+Ancient Sanctuary (AST), 
+Soul of the Duelist (SOD), 
+Dark Beginning 1 (DB1), 
+Rise of Destiny (RDS), 
+Flaming Eternity (FET), 
+Dark Revelation 1 (DR1), 
+The Lost Millennium (TLM), 
+Dragon’s Roar (SD1), 
+Zombie Madness (SD2), 
+Blaze of Destruction (SD3), 
+Fury from the Deep (SD4), 
+Dark Beginning 2 (DB2). 
+
+*/
+
+
+
+/*
+
+<section className='boosterPack' id='LOB'>
                     
                     <div className='boosterInfo'>
                         <h1> Legend of Blue Eyes White Dragon (LOB) </h1>
@@ -70,42 +168,5 @@ function GOATFormatPage () {
                 </section>
 
 
-            </div>
-
-        </div>
-
-    );
-}
-
-
-export default GOATFormatPage; // I will export to App.js
-
-
-/*Legal Sets: 
-
-Legend of Blue Eyes White Dragon (LOB), 
-Yugi and Kaiba Starter Decks (SDY/SDK), 
-Metal Raiders (MRD), Magic/Spell Ruler (MRL/SRL), 
-Pharaoh's Servant (PSV), 
-Joey and Pegasus Starter Decks (SDJ/SDP), 
-Labyrinth of Nightmare (LON), 
-Legacy of Darkness (LOD), 
-Pharaonic Guardian (PGD), 
-Magician's Force (MFC), 
-Dark Crisis (DCR), 
-Yugi and Kaiba Evolution Decks (SYE/SKE), 
-Invasion of Chaos (IOC), 
-Ancient Sanctuary (AST), 
-Soul of the Duelist (SOD), 
-Dark Beginning 1 (DB1), 
-Rise of Destiny (RDS), 
-Flaming Eternity (FET), 
-Dark Revelation 1 (DR1), 
-The Lost Millennium (TLM), 
-Dragon’s Roar (SD1), 
-Zombie Madness (SD2), 
-Blaze of Destruction (SD3), 
-Fury from the Deep (SD4), 
-Dark Beginning 2 (DB2). 
 
 */
